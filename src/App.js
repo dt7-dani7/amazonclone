9033c8ce7afa4b9b6cb03d8ce2a8e37730ed878f
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-01-14 11:16:08 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Danish Thomas we are here</h1>
+      <Header/>
+      {/* Home */ }
     </div>
   );
 }
