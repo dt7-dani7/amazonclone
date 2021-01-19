1af34348049fd55dc19e5141b230172ad54d184f
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-01-19 13:33:10 +0530

    remove basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Checkout.css";
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
@@ -10,6 +13,15 @@ function Checkout() {
                 alt=""/>
                 <div>
                     <h2 className="checkout_title">Your Shopping Basket</h2>
+                    {basket.map(item => (
+                        <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                    ))}
                     {/** */}
                     {/** */}
                     {/** */}
